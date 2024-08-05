5f4caff2bce593ebfd9d75b598eb6b41d7304177
Benjamin <binemugha@gmail.com>
2024-08-05 06:47:14 +0100

    Initial Commit

---
+++ MiniShoppingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniShoppingApp
+//
+//  Created by Benjamin Inemugha  on 05/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniShoppingApp/MiniShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniShoppingAppApp.swift
+//  MiniShoppingApp
+//
+//  Created by Benjamin Inemugha  on 05/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MiniShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
