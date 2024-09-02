d5ffbebb61ca880635144244f5771a182ad6b162
Benjamin <binemugha@gmail.com>
2024-09-02 09:08:07 +0100

    Build full Home screen

--- MiniShoppingApp/HomeScreenViews/HomeScreenView.swift
+++ MiniShoppingApp/HomeScreenViews/HomeScreenView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct HomeScreenView: View {
     var body: some View {
-        Text("Home screen view")
-            .font(.system(size: 40))
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                Image(systemName: "line.3.horizontal")
+                Spacer()
+                Image(systemName: "cart.badge.plus")
+            }
+            .font(.system(.title3))
+
+            Text("Hey")
+                .font(.title)
+                .fontWeight(.bold)
+            Text("Find fresh fruits that you want")
+
+            RoundedRectangle(cornerRadius: 15)
+                .fill(.gray.opacity(0.3))
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search fresh fruits")
+                            .foregroundStyle(.gray)
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                    }.padding()
+                ).frame(width: 350, height: 50, alignment: .center)
+
+            Text("Top Selling")
+                .font(.title)
+                .fontWeight(.bold)
+            TopSellingView()
+
+            Text("Near you")
+                .font(.title)
+                .fontWeight(.bold)
+            NearYouView()
+            Spacer()
+        }.padding()
+            .navigationBarTitle("")
+            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
     }
 }
 
--- MiniShoppingApp/OnBoardingScreenView.swift
+++ MiniShoppingApp/OnBoardingScreenView.swift
@@ -33,6 +33,9 @@ struct OnBoardingScreenView: View {
                 }
                 Spacer()
             }
+
+            .navigationTitle("")
+            .navigationBarHidden(false)
         }
     }
 }
